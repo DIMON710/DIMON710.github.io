a910cc3363396a18ab0340fb5d305c3b6a90903e
DIMON710 <040604merenko4@gmail.com>
2022-06-30 17:03:42 +0300

    spa

--- js/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-let sn = prompt("Иди нахуй");
-let fn = prompt("Имя");
-let eb = prompt("Год рождения");
-let old = (2022-eb)
-console.log("Фамилия: " + sn);
-console.log("Имя: " + fn);
-console.log("Возраст: " + old);
