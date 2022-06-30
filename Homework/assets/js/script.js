0592f1e2d872061e5af9dd08e8ee5f8594e1952d
DIMON710 <040604merenko4@gmail.com>
2022-06-30 16:43:16 +0300

    js_site

---
+++ Homework/assets/js/script.js
@@ -0,0 +1,7 @@
+let sn = prompt("Фамилия");
+let fn = prompt("Имя");
+let eb = prompt("Год рождения");
+let old = (2022-eb)
+console.log("Фамилия: " + sn);
+console.log("Имя: " + fn);
+console.log("Возраст: " + old);
